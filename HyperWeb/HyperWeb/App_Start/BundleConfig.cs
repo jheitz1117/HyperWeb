8259ab9d40b34152696ffe1690b055439ead3590
Josh Heitz <jheitz1117@gmail.com>
2015-08-29 15:37:51 -0500

    Remove extra project. Improve HyperWeb structure.

--- HyperWeb/HyperWeb/App_Start/BundleConfig.cs
+++ HyperWeb/HyperWeb/App_Start/BundleConfig.cs
@@ -7,15 +7,21 @@ namespace HyperWeb
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            // We never bundle javascript
-
-            // Always bundle CSS
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/Styles/bootstrap.css",
-                      "~/Content/Styles/site.css"));
-
-            // force CSS to be compiled into a single CSS file. Set to false to allow each individual CSS file to be loaded separately to make debugging easier.
+            // Enable bundling optimizations, even when the site is in debug mode or local.
             BundleTable.EnableOptimizations = true;
+
+            // We never bundle javascript (require.js makes this a non-issue)
+
+            // Bundle our icons separate from our other stylesheets, since icons are large files and change rarely
+            // If our regular styles change, don't want to force the user to re-download all the large files too.
+            bundles.Add(
+                new StyleBundle("~/bundles/icons")
+                    .Include("~/Content/Styles/Components/font-awesome.css")
+            );
+            bundles.Add(
+                new StyleBundle("~/bundles/styles")
+                    .Include("~/Content/Styles/site.css")
+            );
         }
     }
 }
