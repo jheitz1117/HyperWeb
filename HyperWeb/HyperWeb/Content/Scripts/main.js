eeb2e67661bcc0f3f1ffcf70ca830db91e5e8f1a
Josh Heitz <jheitz1117@gmail.com>
2015-09-01 09:51:48 -0500

    Track active nav item.

--- HyperWeb/HyperWeb/Content/Scripts/main.js
+++ HyperWeb/HyperWeb/Content/Scripts/main.js
@@ -1,1 +1,7 @@
-﻿
+﻿require(['jquery'], function ($) {
+    $("#site-nav .nav.navbar-nav li>a").click(function () {
+        // Setting the active nav item immediately makes the site appear more responsive
+        $("#site-nav .nav.navbar-nav li").removeClass("active");
+        $(this).parent("li").addClass("active");
+    });
+});
